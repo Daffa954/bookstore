f6e32c0ff4d503197efa0a8ed1e0aae06738da49
Daffa954 <dkhoirul05@gmail.com>
2024-05-24 04:03:55 +0700

    menambahkan

---
+++ result.php
@@ -0,0 +1,20 @@
+<?php
+require "controller.php"; 
+
+
+$book = seeDetails($_GET['id']);
+var_dump($book);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
